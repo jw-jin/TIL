fcf2df7ef79a71a63cdd3817584ca0541403246a
우진 <woojin9812071207@gmail.com>
2023-03-10 20:20:38 +0900

    boj

--- BOJ/1074.py
+++ BOJ/1074.py
@@ -1,66 +1,33 @@
 N,r,c = map(int,input().split())
 
-def zcnt(num):
-    global board,move,cnt,move_range
+def recur(n,N,x1,x2,y1,y2):
+    global res, r, c
+    xmid = (x1+x2) // 2
+    ymid = (y1+y2) // 2
+    
+    if n == 1:
+        return 
 
-    if num == 1:
-        for m in move_range:
-            xst = m[0][0]
-            xed = m[0][1]
-            yst = m[1][0]
-            yed = m[1][1]
-            for i in range(yst,yed):
-                    for j in range(xst,xed):
-                        if(board[i][j] == -1):
-                            for m in move:
-                                board[i+m[0]][j+m[1]] = cnt
-                                cnt+=1
-    else: 
-        zcnt(num-1)            
+    
+    if (r<ymid and c<xmid):
+        nx1 = x1; nx2 = xmid; ny1 = y1; ny2 = ymid;
+    elif (r<ymid and c>=xmid):
+        res += (4**(N-1))* 1
+        nx1 = xmid; nx2 = x2; ny1 = y1; ny2 = ymid;
+    elif (r>=ymid and c<xmid):
+        res += (4**(N-1))* 2
+        nx1 = x1; nx2 = xmid; ny1 = ymid; ny2 = y2;
+    elif (r>=ymid and c>=xmid):
+        nx1 = xmid; nx2 = x2; ny1 = ymid; ny2 = y2;
+        res += (4**(N-1))* 3
+    
+    N-=1
+    recur(n//2,N,nx1,nx2,ny1,ny2)
 
+# 4** (n-2)
+# 2 ** N
+n = 2**N
+res = 0
+recur(n,N,0,n,0,n)
 
-
-board = [[-1] * (2 ** N) for _ in range(2 ** N)]
-move = [[0,0],[0,1],[1,0],[1,1]]
-x= int(2**(N-1))
-x2 = int((2**(N-1)*2)-1)
-move_range = [[[0,x-1],[0,x-1]],[[x,x2],[0,x-1]],[[0,x-1],[x,x2]],[[x,x2],[x,x2]]]
-
-#move_range = [[0,2**(N-1)-1],[2**(N-1),(2**(N-1)*2)-1]]
-
-print((2**15)**2)
-cnt = 0 
-st= 0
-ed= st+ 2**(N-1) 
-zcnt(N)
-for i in board:
-    print(i)
-
-# st= ((2**(N))//2) 4사분면
-# ed= st+ 2**(N-1) 
-# print(st,ed)
-# for i in range(st,ed):
-#     for j in range(st,ed):
-#         if(board[i][j] == 0):
-#             for m in move:
-#                 board[i+m[0]][j+m[1]] = cnt
-#                 cnt+=1
-
-# for i in board:
-#     print(i)
-
-
-"""
-00 01
-10 11
-# st= 0 1사분면
-# ed= st+ 2**(N-1) 
-# st= ((2**(N))//2) 4사분면
-# ed= st+ 2**(N-1) 
-# st= ((2**(N))//2) 4사분면
-# ed= st+ 2**(N-1) 
-# st= ((2**(N))//2) 4사분면
-# ed= st+ 2**(N-1) 
-
-
-"""
+print(res)
